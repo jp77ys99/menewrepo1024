f42266963291f5715e630e6a8d16d610c3ebffe1
unknown <jp77ys99@naver.com>
2022-10-24 17:25:43 +0900

    add

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wqd_tlvg@7_8^rj3zc-h6o1!@59ybie7=ezeb-stx12)d6-bwf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'idol',
+        'USER': 'root',
+        'PASSWORD': 'dkagh1.',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
